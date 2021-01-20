016162a44ec7bbf407b235af6d6cc3710bfd4ade
DavidsJulio <davidsjulio97@gmail.com>
2021-01-20 23:07:51 +0000

    Window - create

--- src/application/Java3D.java
+++ src/application/Java3D.java
@@ -1,10 +1,39 @@
 package application;
 
-public class Java3D {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.GraphicsConfiguration;
+import java.awt.event.WindowEvent;
+
+import javax.media.j3d.Canvas3D;
+
+import com.sun.j3d.utils.universe.SimpleUniverse;
+
+public class Java3D extends Frame{
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		
+		Frame frame = new Java3D();
+		frame.setPreferredSize(new Dimension(400, 400));
+		frame.setTitle("Java3D - Project");
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
+	
+	
+	protected void processWindowEvent(WindowEvent e) {
+		super.processWindowEvent(e);
+		if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+			System.exit(0);
+		}
+	}
+	
+	public Java3D() {
+		
+		
+	}
+	
 
 }
